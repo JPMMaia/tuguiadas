bb56936ed6c0778be1aa0d870bbc09b2e9c68013
Diogo Barroso <ei11105@fe.up.pt>
2017-07-22 17:56:27 +0100

    - Adding market content.

---
+++ Assets/Market/Item.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Item : MonoBehaviour
+{
+    string Name = "Apple";
+    string Culture = "Japan";
+    int ValueBuy = 50;
+    int ValueSell = 40;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.GetChild(0).GetComponent<Text>().text = Name;
+        if (transform.parent.name == "Inventory")
+            transform.GetChild(1).GetComponent<Text>().text = ValueSell.ToString();
+        else if (transform.parent.name == "Market")
+            transform.GetChild(1).GetComponent<Text>().text = ValueBuy.ToString();
+    }
+
+    void TaskOnClick()
+    {
+        transform.parent.GetComponent<InventoryManager>().SetSelected(this.gameObject);
+    }
+}
---
+++ Assets/Market/MarketManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MarketManager : MonoBehaviour {
+
+    GameObject inv;
+    GameObject mark;
+
+	// Use this for initialization
+	void Start ()
+    {
+        inv = GameObject.Find("Inventory");
+        mark = GameObject.Find("Market");
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    void GoLeft()
+    {
+        inv.GetComponent<InventoryManager>().GetSelected().transform.SetParent(mark.transform);
+    }
+
+    void GoRight()
+    {
+        mark.GetComponent<InventoryManager>().GetSelected().transform.SetParent(inv.transform);
+    }
+}
