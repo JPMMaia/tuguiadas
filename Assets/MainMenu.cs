b122a80b8c844625805b77ec81233a19b1f5ec48
Miguel Mendes <zephiro309@gmail.com>
2017-07-23 18:53:18 +0100

    Added *cough*working main menu.

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButtonDown(0)) {
+			if(this.transform.childCount == 1)
+				SceneManager.LoadScene("Intro");//switch scene
+			else
+				Destroy(this.transform.GetChild(this.transform.childCount - 1).gameObject);
+		}
+	}
+}
