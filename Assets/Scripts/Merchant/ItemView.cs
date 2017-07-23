4bc7ff6f31ad77a34af244b5f9620a2687af1376
jpmmaia <jpmmaia@gmail.com>
2017-07-23 13:22:25 +0100

    - Linking port and merchant.

---
+++ Assets/Scripts/Merchant/ItemView.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Merchant
+{
+    public class ItemView : MonoBehaviour
+    {
+        public Text NameText;
+        public Text MaterialText;
+        public Text QuantityText;
+        public Text ValueText;
+    }
+}
